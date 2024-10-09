dcc2cf80a94cda5bd2331ad00b1a814b39274c65
Jason Benn <jasoncbenn@gmail.com>
2024-10-09 10:56:41 -0700

    Mochi API

--- apis.py
+++ apis.py
@@ -1,6 +1,16 @@
+from enum import Enum
+import json
+from flask import request
 from flask import Flask, jsonify
 import os
 import random
+
+import requests
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -24,6 +34,48 @@ def random_text():
     else:
         text = get_random_slice("scraped")
     return jsonify({"text": text})
+
+
+@app.route("/api/echo", methods=["GET", "POST"])
+def echo():
+    # Print entire HTTP request
+    print("Request Method:", request.method)
+    print("Request Headers:", request.headers)
+    print("Request URL:", request.url)
+    print("Request Body:", request.get_data(as_text=True))
+    # Return a 201 status code
+    return "", 201
+
+
+class DeckIds(Enum):
+    MACHINE_LEARNING = "rJvG6PtI"
+    DISTRIBUTED_SYSTEMS = "dvQBuvKL"
+    OPERATING_SYSTEMS = "7NCM3TQE"
+    SYSTEM_DESIGN = "GOSfjUYP"
+    PYTHON = "TWEo8eCJ"
+    JAVASCRIPT = "vVo0I5hQ"
+    NETWORKING = "PfIYqVVy"
+    ARCHITECTURE = "MNDlT8v4"
+    DATABASES = "jn4udgEj"
+    PYTORCH = "cRm6jshY"
+
+
+@app.route("/api/create-card", methods=["GET", "POST"])
+def create_card():
+    data = json.loads(request.get_data(as_text=True))
+    if data.get("deck") not in [deck.name for deck in DeckIds]:
+        return jsonify({"error": "Invalid deck"}), 400
+    deck_id = DeckIds[data["deck"]].value
+
+    mochi_data = {"content": data.get("front") + "\n---\n" + data.get("back"), "deck-id": deck_id}
+    response = requests.post("https://app.mochi.cards/api/cards", json=mochi_data, auth=(os.getenv("MOCHI_API_KEY"), ""))
+
+    print("Mochi API Response:", response.text)
+    print("Mochi API Status Code:", response.status_code)
+
+    if response.status_code not in [201, 200]:
+        return jsonify({"error": "Failed to create card"}), 500
+    return jsonify({"message": "Card created successfully"}), 201
 
 
 @app.route("/api/privacy", methods=["GET"])
